1d6f3e68e15171ed6ec7de0d1d6f300a8b4f581c
Mariame Barry <bmariame38@gmail.com>
2021-08-26 11:12:32 +0200

    update mapping

--- src/main/java/com/ussd/app/Ussd/controller/UserTransactionController.java
+++ src/main/java/com/ussd/app/Ussd/controller/UserTransactionController.java
@@ -4,10 +4,7 @@ import com.ussd.app.Ussd.repository.*;
 import com.ussd.app.Ussd.utils.Jours;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/sante/ussd")
@@ -27,7 +24,7 @@ public class UserTransactionController {
 
 
 
-    @GetMapping
+    @PostMapping
 
     public ResponseEntity<String> processUssd(@RequestParam(name = "sessionId") String sessionId,
                                               @RequestParam(name = "phoneNumber") String msisdn,
