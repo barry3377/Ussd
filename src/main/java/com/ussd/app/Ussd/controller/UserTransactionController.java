788604edba3f335cdb71dbdd18be1057e378c55d
Mariame Barry <bmariame38@gmail.com>
2021-08-27 10:40:02 +0200

    ussd

--- src/main/java/com/ussd/app/Ussd/controller/UserTransactionController.java
+++ src/main/java/com/ussd/app/Ussd/controller/UserTransactionController.java
@@ -44,12 +44,12 @@ public class UserTransactionController {
             return getMenu("services", input);
         }else if(input.matches("^1\\*[0-9]+\\*[0-9]+")
                 || input.matches("^2\\*6[0-9]{8}\\*[0-9]+\\*[0-9]+")||
-                input.matches("^3\\*1\\*[1-7]")
+                input.matches("^3\\*1")
                         || input.matches("^3\\*2\\*6[0-9]{8}") ){
             return getMenu("jours", input);
         }else if((input.matches("^1\\*[0-9]+\\*[0-9]+\\*[1-7]")
                 || input.matches("^2\\*6[0-9]{8}\\*[0-9]+\\*[0-9]+\\*[1-7]")||
-                (input.matches("^3\\*1\\*[1-7]\\*[0-9]+") ||
+                (input.matches("^3\\*1\\*[1-7]+") ||
                         (input.matches("^3\\*2\\*6[0-9]{8}\\*[0-9]+"))))){
             return getMenu("heures", input);
         }else if(( input.matches("^1\\*[0-9]+\\*[0-9]+\\*[1-7]\\*[0-9]+")
