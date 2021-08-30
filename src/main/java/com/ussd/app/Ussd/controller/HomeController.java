2ff46cdee27f53880d5e1aa5155883b8c39e5f23
Mariame Barry <bmariame38@gmail.com>
2021-08-30 15:15:37 +0200

    update

--- src/main/java/com/ussd/app/Ussd/controller/HomeController.java
+++ src/main/java/com/ussd/app/Ussd/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.ussd.app.Ussd.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +11,7 @@ public class HomeController {
     }
     @RequestMapping(value="/lout")
     public  String login(){
-        return "login.html";
+        return "templates/login.html";
     }
 
 }
