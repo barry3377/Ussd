7f91a8cfcb687e118b01129d27188d853a68daf5
Mariame Barry <bmariame38@gmail.com>
2021-09-01 10:59:22 +0200

    update

--- src/main/java/com/ussd/app/Ussd/UssdApplication.java
+++ src/main/java/com/ussd/app/Ussd/UssdApplication.java
@@ -21,18 +21,20 @@ JoursRepository joursRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		Jour jour=new Jour("Mardi");
-	joursRepository.save(jour);
-		Jour jour1=new Jour("Mercredi");
-		joursRepository.save(jour1);
-		Jour jour2=new Jour("Jeudi");
-		joursRepository.save(jour2);
-		Jour jour3=new Jour("Vendredi");
-		joursRepository.save(jour3);
-		Jour jour4=new Jour("Samedi");
-		joursRepository.save(jour4);
-		Jour jour5=new Jour("Dimanche");
-		joursRepository.save(jour5);
+//		Jour jour=new Jour("Lundi");
+//		joursRepository.save(jour);
+//		Jour jour0=new Jour("Mardi");
+//	joursRepository.save(jour0);
+//		Jour jour1=new Jour("Mercredi");
+//		joursRepository.save(jour1);
+//		Jour jour2=new Jour("Jeudi");
+//		joursRepository.save(jour2);
+//		Jour jour3=new Jour("Vendredi");
+//		joursRepository.save(jour3);
+//		Jour jour4=new Jour("Samedi");
+//		joursRepository.save(jour4);
+//		Jour jour5=new Jour("Dimanche");
+//		joursRepository.save(jour5);
 		heureRepository.findAll().forEach(heure -> {
 			System.out.println(heure.toString());
 		});
