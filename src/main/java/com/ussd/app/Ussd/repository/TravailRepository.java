7291c3d4818de44f2306ea6cdd9c38540f3c741c
Mariame Barry <bmariame38@gmail.com>
2021-09-22 12:16:27 +0000

    update

--- src/main/java/com/ussd/app/Ussd/repository/TravailRepository.java
+++ src/main/java/com/ussd/app/Ussd/repository/TravailRepository.java
@@ -20,7 +20,7 @@ public interface TravailRepository extends JpaRepository<Travail,Long > {
     @Query(value = "SELECT * FROM travail WHERE hopital_id = ?1 AND departement_id = ?2 GROUP BY heure_id", nativeQuery = true)
     List<Travail> getByHopitalServiceAndJour(long id, long service);
 
-    @Query(value = "SELECT DISTINCT ON(departement_id) departement_id,jour_id,heure_id,hopital_id FROM travail ORDER BY departement_id  ", nativeQuery = true)
+    @Query(value = "SELECT * FROM travail GROUP BY departement_id ", nativeQuery = true)
     List<Travail> getGroup();
 
     @Query(value = "SELECT * FROM travail WHERE travail.departement_id = ?1 GROUP BY hopital_id", nativeQuery = true)
