dc8be25ce118622e5e6177149fdc610359d0ed2e
Mariame Barry <bmariame38@gmail.com>
2021-09-22 12:37:00 +0000

    update

--- src/main/java/com/ussd/app/Ussd/utils/ITravail.java
+++ src/main/java/com/ussd/app/Ussd/utils/ITravail.java
@@ -1,7 +1,9 @@
-//package com.ussd.app.Ussd.utils;
-//
-//public interface ITravail {
-//    Long getId();
-//    Long getDepartementId();
-//    Long getJourId();
-//}
+package com.ussd.app.Ussd.utils;
+
+public interface ITravail {
+    Long getId();
+    Long getDepartementId();
+    Long getJourId();
+    Long getHeure();
+    Long getHopital();
+}
