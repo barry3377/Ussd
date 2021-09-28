f790419c38726dd841748ff14c335523c8e210e4
barry3377 <bmariame38@gmail>
2021-09-28 11:04:33 +0000

    update

--- src/main/java/com/ussd/app/Ussd/OrangeSMS/OrangeSMS.java
+++ src/main/java/com/ussd/app/Ussd/OrangeSMS/OrangeSMS.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class OrangeSMS {
 
     public  OkHttpClient client;
-@Autowired
+
     JSONParser parser;
     public OrangeSMS() {
         client = new OkHttpClient();
