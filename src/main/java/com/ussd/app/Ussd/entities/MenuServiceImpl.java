29758143aa015f3a76e93b9473432429f3b3a34e
barry3377 <bmariame38@gmail>
2021-10-04 09:59:47 +0000

    update

--- src/main/java/com/ussd/app/Ussd/entities/MenuServiceImpl.java
+++ src/main/java/com/ussd/app/Ussd/entities/MenuServiceImpl.java
@@ -393,14 +393,14 @@ public class MenuServiceImpl implements MenuService {
 
             Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(date);
 
-//            RendezVous rendezVous = new RendezVous();
-//            rendezVous.setDepartement(dep);
-//            rendezVous.setHopital(hopital);
-//            rendezVous.setDate(date1);
-//            rendezVous.setHeures(heure);
-//            rendezVous.setTicket(ticket);
-//            rendezVous.setUserTransaction(user);
-//            rendezVous =  rendezVousRepository.save(rendezVous);
+            RendezVous rendezVous = new RendezVous();
+            rendezVous.setDepartement(dep);
+            rendezVous.setHopital(hopital);
+            rendezVous.setDate(date1);
+            rendezVous.setHeures(heure);
+            rendezVous.setTicket(ticket);
+            rendezVous.setUserTransaction(user);
+            rendezVous =  rendezVousRepository.save(rendezVous);
 //            String message = "Votre rendez-vous  a bien ete enregistre, votre numero d'enregistrement est "+ticket;
 //            boolean b = orangeSMS.sendMessage("+224"+num, message);
             return "END Votre rendez-vous pour une autre personne à ete enregistrer," +
