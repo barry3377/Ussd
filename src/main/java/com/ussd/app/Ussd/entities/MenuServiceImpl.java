5f68d5cae2d93467cb275c0362d9416e4503d2fd
barry3377 <bmariame38@gmail>
2021-10-04 10:19:31 +0000

    update

--- src/main/java/com/ussd/app/Ussd/entities/MenuServiceImpl.java
+++ src/main/java/com/ussd/app/Ussd/entities/MenuServiceImpl.java
@@ -393,16 +393,16 @@ public class MenuServiceImpl implements MenuService {
 
             Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(date);
 
-//            RendezVous rendezVous = new RendezVous();
-//            rendezVous.setDepartement(dep);
-//            rendezVous.setHopital(hopital);
-//            rendezVous.setDate(date1);
-//            rendezVous.setHeures(heure);
-//            rendezVous.setTicket(ticket);
-//            rendezVous.setUserTransaction(user);
-//            rendezVous =  rendezVousRepository.save(rendezVous);
-//            String message = "Votre rendez-vous  a bien ete enregistre, votre numero d'enregistrement est "+ticket;
-//            boolean b = orangeSMS.sendMessage("+224"+num, message);
+            RendezVous rendezVous = new RendezVous();
+            rendezVous.setDepartement(dep);
+            rendezVous.setHopital(hopital);
+            rendezVous.setDate(date1);
+            rendezVous.setHeures(heure);
+            rendezVous.setTicket(ticket);
+            rendezVous.setUserTransaction(user);
+            rendezVous =  rendezVousRepository.save(rendezVous);
+            String message = "Votre rendez-vous  a bien ete enregistre, votre numero d'enregistrement est "+ticket;
+            boolean b = orangeSMS.sendMessage("+224"+num, message);
             return "END Votre rendez-vous pour une autre personne à ete enregistrer," +
                     " vous recevrer un sms de confirmation"+"Status: "+telephone + dep +date1 +heure+ticket+hopital;
 
